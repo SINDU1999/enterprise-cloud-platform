d645a5b98c9a201ff8b59251f769fcefa9c67aea
Your Name <your-email@example.com>
2026-07-27 16:01:52 +0530

    Add VPC and multi-AZ public/private subnets

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,7 +1,18 @@
-############################################
-# FinCore Enterprise Cloud Platform
-# Development Environment
-############################################
+module "vpc" {
 
-# Terraform modules will be added here
-# starting with the VPC module.
+  source = "../../modules/vpc"
+
+  project_name = var.project_name
+  environment  = var.environment
+  vpc_cidr     = var.vpc_cidr
+
+  public_subnet_1_cidr = var.public_subnet_1_cidr
+  public_subnet_2_cidr = var.public_subnet_2_cidr
+
+  private_subnet_1_cidr = var.private_subnet_1_cidr
+  private_subnet_2_cidr = var.private_subnet_2_cidr
+
+  availability_zone_1 = var.availability_zone_1
+  availability_zone_2 = var.availability_zone_2
+}
+  
--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -17,3 +17,32 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
+variable "public_subnet_1_cidr" {
+  description = "CIDR block for Public Subnet 1"
+  type        = string
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for Public Subnet 2"
+  type        = string
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR block for Private Subnet 1"
+  type        = string
+}
+
+variable "private_subnet_2_cidr" {
+  description = "CIDR block for Private Subnet 2"
+  type        = string
+}
+
+variable "availability_zone_1" {
+  description = "Primary Availability Zone"
+  type        = string
+}
+
+variable "availability_zone_2" {
+  description = "Secondary Availability Zone"
+  type        = string
+}
